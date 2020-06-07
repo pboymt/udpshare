8444de094a64800ef76259e2464442a020ca68b4
Robert Lee <pboymt@foxmail.com>
2020-06-07 23:16:55 +0800

    response

--- src/index.ts
+++ src/index.ts
@@ -37,33 +37,34 @@ if (process.argv.includes('--client')) {
     mdns.on('query', function (query, rinfo) {
         console.log('got a query packet:', query, rinfo)
 
-    })
+        const ifaces = networkInterfaces();
+        const lan_ips: string[] = [];
 
-    const ifaces = networkInterfaces();
-    const lan_ips: string[] = [];
-
-    for (const iface in ifaces) {
-        if (ifaces.hasOwnProperty(iface)) {
-            const infos = ifaces[iface];
-            if (infos) {
-                for (const info of infos) {
-                    if (!info.internal && !['255.255.255.255', 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff'].includes(info.netmask)) {
-                        lan_ips.push(info.address);
+        for (const iface in ifaces) {
+            if (ifaces.hasOwnProperty(iface)) {
+                const infos = ifaces[iface];
+                if (infos) {
+                    for (const info of infos) {
+                        if (!info.internal && !['255.255.255.255', 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff'].includes(info.netmask)) {
+                            lan_ips.push(info.address);
+                        }
                     }
                 }
             }
         }
-    }
 
-    mdns.respond({
-        answers: lan_ips.map(ip => {
-            return {
-                name: 'share.pboymt.local',
-                type: 'A',
-                data: ip
-            }
+        mdns.respond({
+            answers: lan_ips.map(ip => {
+                return {
+                    name: 'share.pboymt.local',
+                    type: 'A',
+                    data: ip
+                }
+            })
         })
     })
+
+
 }
 
 
